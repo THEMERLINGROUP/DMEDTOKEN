8e334a47d7fbabf990d39aa40559317a3e16f2e2
THEMERLINGROUP <67569156+THEMERLINGROUP@users.noreply.github.com>
2021-08-17 17:11:11 -0400

    Updated comments in Test, connected to Ether.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let ethers = require('ethers')
 const main = async () => {
     const TOKEN_ADDRESS = '0x58B4b7bF769c648fb03eCbA2Aefa223Bce0Ea208'
-    let PrivateKey1 = new ethers.Wallet.fromPrivateKey1(process.env.PrivateKey1);
-    let PrivateKey2 = new ethers.Wallet.fromPrivateKey2(process.env.PrivateKey2);
-    let provider = ethers.getDefaultProvider('ganache')
-
+    let wallet1 = new ethers.Wallet.fromPrivateKey1(process.env.PrivateKey1);
+    let wallet2 = new ethers.Wallet.fromPrivateKey2(process.env.PrivateKey2);
+    let provider = ethers.getDefaultProvider('ganache');
+    wallet1 = privateKey1.connect(provider);
+    wallet2 = privateKey2.connect(provider);
+    let abi = [
+        'function balanceOf(address who) external view returns (uint256)',
+        'function transfer(address to, uint256 value) external returns (bool)'
+    ] 
+    let token = new ethers.Contract(TOKEN_ADDRESS, abi, wallet1)
+    let tx = await token.faucet()
+    await tx.wait()
+    let amount1 = await token.balanceOf(wallet1.address)
+    let amount2 = await token.balanceOf(wallet2.address)
+    console.log('Balance #1: ', ethers.utils.formatEther(amount1))
+    console.log('Balance #2: ', ethers.utils.formatEther(amount2))
+    tx = await token.transfer(wallet2.address, ethers.utils.parseEther('100'))
+    await tx.wait()
 }
--- test/dmedTokenTest.js
+++ test/dmedTokenTest.js
@@ -1,10 +1,17 @@
-const {expectRevert, expectEvent} = require("@openzeppelin/test-helpers");
-const Token = artifacts.require("DECENTMEDtoken.sol");
 //Template
  /// @author Samuel Salgado
-    /// @notice Creation of token and its initial components(Name, symbol, decimals, and supply)
+ /// @title DECENTMEDtoken
+    /// @notice Creation of token and its prequisite events and functions 
     /// @dev Using natSpec for commenting of Tests
-    /// @param rings Several tests for the Token
+
+const {expectRevert, expectEvent} = require("@openzeppelin/test-helpers");
+const Token = artifacts.require("DECENTMEDtoken.sol");
+
+
+
+
+    /// @notice using an async call, you are creating a a token
+    /// @param Token consists of a name, symbol, # of decimals, and supply
     /// @return The creation of an ERC20 Token  
 contract("DECENTMEDtoken", (accounts) => {
     let token;
@@ -14,19 +21,15 @@ contract("DECENTMEDtoken", (accounts) => {
         token = await Token.new("DecentMed", "DMED", 18, initialBalance);
     
     });
-     /// @author Samuel Salgado
-    /// @notice Must have a burn function post-sending of funds
-    /// @dev Include burn function()
+  
+    /// @notice Burn function post-sending of funds
     /// @param rings The value of token and which account is sending funds
-    /// @return from account and value amount
     it("Must have a burn function and burn from sender account", async () => {
         await token.burn({value: 10});
         await token.burnFrom({from: accounts[0],amount:10});
 
     });
-     /// @author Samuel Salgado
-    /// @notice will revert if burn amount exceeds allowance
-    /// @dev Assert that CurrentAllowance is a number
+    
     /// @param rings The sender, amount, and from address
     /// @return that current allowance should equal amount
     it("burn amount should not exceed current allowance", async() => {
@@ -38,16 +41,12 @@ contract("DECENTMEDtoken", (accounts) => {
         );
         assert(currentAllowance.toNumber() === 10); 
     });
-     /// @author Samuel Salgado
     /// @notice Contract is a contract
-    /// @dev Standard function that contract is an account
-    /// @param rings the contract is a contract
-    /// @return a contract
+    
     it("Must be a contract", async () => {
         await token.isContract(account);
     });
-    /// @author Samuel Salgado
-    /// @notice Contract should transfer funds
+    /// @notice Transfer function 
     /// @dev The contract should transfer tokens from a sender 
     /// @param rings a sender and a an amount of token
     /// @return a balance that equals the amount
@@ -58,7 +57,6 @@ contract("DECENTMEDtoken", (accounts) => {
         const balance = await token.balances(sender);
         assert(balance.toNumber() === 100);
     });
-    /// @author Samuel Salgado
     /// @notice Transfer cannot be be to self 
     /// @dev The contract will revert if funds are sent to self
     /// @param rings a sender, recipient, and transfer amount
@@ -74,7 +72,6 @@ contract("DECENTMEDtoken", (accounts) => {
              }),
              "Transfer to zero address"
     });
-    /// @author Samuel Salgado
     /// @notice Transfer will be from one adress to another
     /// @dev A revert is put in place if the from address is the same as the to address
     /// @param rings a sender, recipient, and amount
@@ -90,11 +87,10 @@ contract("DECENTMEDtoken", (accounts) => {
             }),
             "Transfer from zero address"
     });
-    /// @author Samuel Salgado
     /// @notice Should emit an event for transferFrom
     /// @dev Standard event for a transferFrom
     /// @param rings the account, recipient, and transfer(from: sender)
-    /// @return an event for transfer
+    /// @return an event for transferFrom
 
   
     it("should emit event for transferFrom", async() => {
@@ -122,11 +118,9 @@ contract("DECENTMEDtoken", (accounts) => {
             tokens: "20",
         });
     });
-    /// @author Samuel Salgado
-    /// @notice Contract should only transfer if granted approval
-    /// @dev Standard approval function in which transfer is denied if NOT approved
+    /// @dev Contract should only transfer if granted approval
     /// @param rings the recipient account, amount, and from sender account
-    /// @return NOT approved if transfer is NOT approved
+    /// @return Reverts if token is not approved
 
     it("should not transfer token if not approved", async() => {
         await expectRevert(
